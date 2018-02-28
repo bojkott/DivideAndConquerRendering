459b47735c4b8ea8f62b5746181b70820404b44f
Ludvig Arlebrink <ludvig.arlebrink@outlook.com>
2018-02-28 08:49:16 +0100

    Added vertexbufferbinding

--- DivideAndConquerRendering/VertexBuffer.h
+++ DivideAndConquerRendering/VertexBuffer.h
@@ -9,15 +9,20 @@ class VertexBuffer
 private:
 	vk::DeviceMemory vertexBufferMemory;
 	vk::Buffer vertexBuffer;
+	uint32_t nrOfVerts;
 
 	// Pointer to the device that the buffer belongs to
 	vk::Device* device;
-	
+		
 public:
-	VertexBuffer(std::vector<uint32_t>& verts, DeviceContext * device);
+	VertexBuffer(std::vector<uint32_t>& verts, DeviceContext * context);
 	VertexBuffer() {}; // Just to make std::map stfu
 	void Destroy();
 	vk::Buffer& getVertexBuffer();
 	vk::DeviceMemory& getVertexBufferMemory();
+	void BindBuffer(DeviceContext* context) const;
+	void unBindBuffer(DeviceContext* context) const;
+	uint32_t getNrOfVerts() const;
+private:
 
 };
