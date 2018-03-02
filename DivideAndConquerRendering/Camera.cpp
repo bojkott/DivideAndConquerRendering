7644a976d0522e6d357282fff2b60b7efbf96949
Carl Söyseth <carlsoyseth@gmail.com>
2018-03-02 11:46:42 +0100

    started rework of camera

--- DivideAndConquerRendering/Camera.cpp
+++ DivideAndConquerRendering/Camera.cpp
@@ -1,10 +1,8 @@
 #include "Camera.h"
 #include "Renderer.h"
+#include "Window.h"
 
-vk::DescriptorSetLayout Camera::descriptorSetLayout;
-vk::Device* Camera::device = nullptr;
-Buffer Camera::cameraBuffer;
-Camera::UniformBufferObject Camera::cameraObject;
+std::map<DeviceContext*, Camera> Camera::cameraDict;
 
 
 vk::DescriptorSetLayout* Camera::getDescriptorSetLayout()
@@ -12,12 +10,75 @@ vk::DescriptorSetLayout* Camera::getDescriptorSetLayout()
 	return &descriptorSetLayout;
 }
 
-void Camera::Init(DeviceContext* context, float x, float y, float z)  
+Camera Camera::getInstance(DeviceContext* context, float x, float y, float z)  
 {
-	cameraBuffer = Buffer(context,
+	if (cameraDict.find(context) == cameraDict.end())
+	{
+		cameraDict[context] = Camera(context, x, y, z);
+	}
+	return cameraDict[context];
+}
+
+Camera::~Camera()
+{
+	device->destroyDescriptorSetLayout(descriptorSetLayout);
+}
+
+void Camera::update(float dt)
+{
+	int x, int y;
+
+	SDL_GetRelativeMouseState(&x, &y);
+	const Uint8 *state = SDL_GetKeyboardState(NULL);
+	float cameraSpeed = 0.05f * dt;
+
+
+	// Fucking keyboard n shit
+	if (state[SDL_SCANCODE_W])
+		cameraPos += cameraSpeed * cameraFront;
+
+	if (state[SDL_SCANCODE_S])
+		cameraPos -= cameraSpeed * cameraFront;
+
+	if (state[SDL_SCANCODE_A])
+		cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+	if (state[SDL_SCANCODE_D])
+		cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+
+
+	// Fucking mouse fuck
+	float sensitivity = 0.05;
+    y *= sensitivity;
+    x *= sensitivity;
+
+	static int yaw = 0;
+	static int pitch = 0;
+	yaw += x;
+	pitch += y;
+
+	if (pitch > 89.0f)
+		pitch = 89.0f;
+	if (pitch < -89.0f)
+		pitch = -89.0f;
+
+	glm::vec3 front;
+	front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+	front.y = sin(glm::radians(pitch));
+	front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+	cameraFront = glm::normalize(front);
+	
+
+	cameraObject.view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+}
+
+Camera::Camera(DeviceContext * context, float x, float y, float z)
+{
+	cameraBuffer = std::move(Buffer(context,
 		sizeof(UniformBufferObject), vk::BufferUsageFlagBits::eUniformBuffer, vk::SharingMode::eExclusive,
-		vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCoherent);
+		vk::MemoryPropertyFlagBits::eHostVisible | vk::MemoryPropertyFlagBits::eHostCached));
 	vk::DescriptorSetLayoutBinding uboLayoutBinding = {};
+
 	uboLayoutBinding.binding = 0;
 	uboLayoutBinding.descriptorType = vk::DescriptorType::eUniformBuffer;
 	uboLayoutBinding.descriptorCount = 1;
@@ -30,19 +91,20 @@ void Camera::Init(DeviceContext* context, float x, float y, float z)
 	layout.pBindings = &uboLayoutBinding;
 
 	context->getDevice().createDescriptorSetLayout(&layout, nullptr, &descriptorSetLayout);
+	
+	// TODO: sätt den till faktiska mitten
+	SDL_WarpMouseInWindow(Window::window, 400, 300);
 
-	cameraObject.view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-	cameraObject.model = glm::rotate(glm::mat4(1.0f), glm::radians(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-	cameraObject.proj = glm::perspective(glm::radians(45.0f), (float) context->getSwapchain().extent.width / (float) context->getSwapchain().extent.height,
-		0.1f, 10.0f);
+
+	cameraObject.proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
 	cameraObject.proj[1][1] *= -1;
 
 	cameraBuffer.setData(&cameraObject);
 }
 
-void Camera::Destroy()
+void Camera::updateCameraBuffer()
 {
-	if (device != nullptr)
-		device->destroyDescriptorSetLayout(descriptorSetLayout);
+
 }
+
--- DivideAndConquerRendering/Camera.h
+++ DivideAndConquerRendering/Camera.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <map>
 #include <vulkan\vulkan.hpp>
 #include <glm\glm.hpp>
 #include "DeviceContext.h"
@@ -9,27 +10,33 @@ class Camera
 public:
 	struct UniformBufferObject
 	{
-		glm::mat4 model;
 		glm::mat4 view;
 		glm::mat4 proj;
 	};
 
-	static vk::DescriptorSetLayout descriptorSetLayout;
-	static vk::Device* device;
-	static Buffer cameraBuffer;
-	static UniformBufferObject cameraObject;
+
 
 private:
+	vk::DescriptorSetLayout descriptorSetLayout;
+	vk::Device* device;
+	Buffer cameraBuffer;
+	UniformBufferObject cameraObject;
+	static std::map<DeviceContext*, Camera> cameraDict;
 
+	glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+	glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
 
 public:
-	static vk::DescriptorSetLayout* getDescriptorSetLayout();
-	static void Init(DeviceContext* context, float x = 0, float y = 0, float z = 0);
-	static void Destroy();
-	
+	vk::DescriptorSetLayout* getDescriptorSetLayout();
+	static Camera getInstance(DeviceContext* context, float x = 0, float y = 0, float z = 0);
+	~Camera();
+	Camera() {}; // Fuck you map
+	void update(float dt);
+
 
 private:
-
-
+	Camera(DeviceContext* context, float x = 0, float y = 0, float z = 0);
+	void updateCameraBuffer();
 
 };
